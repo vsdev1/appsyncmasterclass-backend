c008f7b9563f4f29a87626807efde1a123f0ba9d
Volker Sommerfeld <volker.sommerfeld@idealo.de>
2021-02-11 20:05:33 +0100

    added e2e test case for getUploadImageUrl

--- __tests__/test_cases/e2e/user-profile.js
+++ __tests__/test_cases/e2e/user-profile.js
@@ -36,6 +36,20 @@ describe('Given an authenticated user', () => {
     expect(profile.screenName).toContain(lastName)
   })
 
+  it('The user can get an URL to upload new profile image', async () => {
+    const uploadUrl = await when.a_user_calls_getImageUploadUrl(user, '.png', 'image/png')
+
+    const bucketName = process.env.BUCKET_NAME
+    const regex = new RegExp(`https://${bucketName}.s3-accelerate.amazonaws.com/${user.username}/.*\.png\?.*Content-Type=image%2Fpng.*`)
+    expect(uploadUrl).toMatch(regex)
+
+    const filePath = path.join(__dirname, '../../data/logo.png')
+    await then.user_can_upload_image_to_url(uploadUrl, filePath, 'image/png')
+
+    const downloadUrl = uploadUrl.split('?')[0]
+    await then.user_can_download_image_from(downloadUrl)
+  })
+
   it('The user can edit his profile with editMyProfile', async () => {
     const newName = chance.first()
     const input = {
